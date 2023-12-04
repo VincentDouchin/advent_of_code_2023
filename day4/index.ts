0e72371901e2d67ffc5fe8ed518ba452dacecf0b
Vincent Douchin <vincent.douchin@gmail.com>
2023-12-04 08:38:31 +0100

    day 4 clean up

--- day4/index.ts
+++ day4/index.ts
@@ -2,13 +2,6 @@ import { readFile } from 'node:fs/promises'
 
 async function main() {
 	const inputFile = await readFile('./day4/input.txt', 'utf-8')
-// 	const inputFile = `
-// 	Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 	const lines = inputFile.split('\n').filter(l => l.length)
 	const linesFormatted = lines.map((l) => {
 		return l.split(':')[1].split('|').map((r) => {
@@ -31,14 +24,14 @@ async function main() {
 		for (let i = 0; i < linesFormatted.length; i++) {
 			const [winning, numbers] = linesFormatted[i]
 			const amount = winning.reduce((total, w) => numbers.includes(w) ? total + 1 : total, 1)
-			for (let a = i + 1; a < i + amount; a++) {
-				if (points[a]) {
-					points[a] += points[i]
+			for (let j = i + 1; j < i + amount; j++) {
+				if (points[j]) {
+					points[j] += points[i]
 				}
 			}
 		}
 		const result = points.reduce((acc, v) => acc + v, 0)
-		console.log(result)
+		console.log(result) // 5923918
 	}
 }
 main()
