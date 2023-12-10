e96ab68e14174b47d7711c8f453143b663da5a44
Vincent Douchin <vincent.douchin@gmail.com>
2023-12-10 23:15:36 +0100

    dirty day 10

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,119 +1,107 @@
-import run from "aocrunner"
+import run from 'aocrunner'
 
 const parseInput = (rawInput: string) => rawInput.split('\n')
-const directions = {
-  n:[0,-1],
-  s:[0,1],
-  e:[1,0],
-  w:[-1,0],
+const directions: Record<'s' | 'n' | 'w' | 'e', [number, number]> = {
+	n: [0, -1],
+	s: [0, 1],
+	e: [1, 0],
+	w: [-1, 0],
 }
-const opposites ={
-  n:'s',
-  s:'n',
-  e:'w',
-  w:'e',
+const opposites = {
+	n: 's',
+	s: 'n',
+	e: 'w',
+	w: 'e',
 }
-const map ={
-  "|":['n','s'],
-  '-':['e','w'],
-  'L':['n','e'],
-  'J':['n','w'],
-  '7':['s','w'],
-  'F':['s','e'],
-  'S':['s','w','n','e'], 
+const map = {
+	'|': ['n', 's'],
+	'-': ['e', 'w'],
+	'L': ['n', 'e'],
+	'J': ['n', 'w'],
+	'7': ['s', 'w'],
+	'F': ['s', 'e'],
+	'S': ['s', 'w', 'n', 'e'],
 }
-const 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput)
-  const start = input.reduce((acc,v,y)=>v.includes('S')?{y,x:v.indexOf('S')}:acc,{y:0,x:0})
-  
-  
-    let lastdir = 's'
-    let last = null
-    let steps =0
-    let lastpos =start
-    
-    while(last !== 'S'){
-      const [newX,newY] =directions[lastdir]
-      const next =input[lastpos.y+newY]?.[lastpos.x+newX]
-      
-      lastdir = map[next].find(x=>x!==opposites[lastdir])
-      steps++
-      lastpos = {x:lastpos.x+newX,y:lastpos.y+newY,s:next}
-      last = next
-      
-    }
-  return steps/2
-    
 
+const part1 = (rawInput: string) => {
+	const input = parseInput(rawInput)
+	const start = input.reduce((acc, v, y) => v.includes('S') ? { y, x: v.indexOf('S') } : acc, { y: 0, x: 0 })
+
+	let lastdir: 's' | 'n' | 'w' | 'e' = 's'
+	let last = null
+	let steps = 0
+	let lastpos = start
+
+	while (last !== 'S') {
+		const [newX, newY] = directions[lastdir]
+		const next = input[lastpos.y + newY]?.[lastpos.x + newX]
+
+		lastdir = map[next].find(x => x !== opposites[lastdir])
+		steps++
+		lastpos = { x: lastpos.x + newX, y: lastpos.y + newY, s: next }
+		last = next
+	}
+	return steps / 2
 }
 
 const part2 = (rawInput: string) => {
-    const input = parseInput(rawInput)
-  const start = input.reduce((acc,v,y)=>v.includes('S')?{y,x:v.indexOf('S')}:acc,{y:0,x:0})
-  const points =[start]
-  
-    let lastdir = 's'
-    let last = null
-    let steps =0
-    let lastpos =start
-    
-    while(last !== 'S'){
-      const [newX,newY] =directions[lastdir]
-      const next =input[lastpos.y+newY]?.[lastpos.x+newX]
-      
-      lastdir = map[next].find(x=>x!==opposites[lastdir])
-      steps++
-      lastpos = {x:lastpos.x+newX,y:lastpos.y+newY}
-      last = next
-      points.push({x:lastpos.x,y:lastpos.y, s :next})
-    }
-  let tiles =0
-  for (let i =0;i<input.length;i++){
-    let t =0
-    let ins = false
-    const p = points.filter(x=>x.y===i)
-    const xx = p.map(x=>x.x)
-    const max = Math.max(...xx)
-    const min = Math.min(...xx)
-    for (let j =0;j<=input[i].length;j++){
-      const fo= p.find(x=>x.x===j)?.s
-      const left = fo && 'LF'.includes(fo)
-      const right = fo&&'7F|'.includes(fo)
-      
-      if(right) ins = !ins
-      const pi = p.filter(x=>x.x<j && x.s == '|').length%2==1
-      if(!fo &&ins) t++
-    
-    }
-    console.log(t)
-    tiles+=t
-  }
-  return tiles
-  
+	const input = parseInput(rawInput)
+	const start = input.reduce((acc, v, y) => v.includes('S') ? { y, x: v.indexOf('S') } : acc, { y: 0, x: 0 })
+	const points = [{ ...start, s: '7' }]
+	console.log(start)
+	let lastdir = 's'
+	let last = null
+	let steps = 0
 
+	let lastpos = { ...start }
+
+	while (last !== 'S') {
+		const [newX, newY] = directions[lastdir]
+		const next = input[lastpos.y + newY]?.[lastpos.x + newX]
+
+		lastdir = map[next].find(x => x !== opposites[lastdir])
+		steps++
+		lastpos = { x: lastpos.x + newX, y: lastpos.y + newY }
+		last = next
+		points.push({ x: lastpos.x, y: lastpos.y, s: next })
+	}
+
+	let tiles = 0
+	for (let i = 0; i < input.length; i++) {
+		let ins = false
+		const p = points.filter(x => x.y === i)
+		for (let j = 0; j < input[i].length - 1; j++) {
+			const fo = p.find(x => x.x === j)?.s
+			const right = fo && ['7', 'F', '|'].includes(fo)
+
+			if (right) ins = !ins
+			if (!fo && ins) tiles++
+		
+	}
+	return tiles
 }
-const example=`
+
+const example = `
 7-F7-
 .FJ|7
 SJLL7
 |F--J
 LJ.LJ`
 run({
-  part1: {
-    tests: [
-       {
-         input: example,
-      expected: 8,
-       },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-       {
-         input: `
-         .F----7F7F7F7F-7....
+	part1: {
+		tests: [
+			{
+				input: example,
+				expected: 8,
+			},
+		],
+		solution: part1,
+	},
+	part2: {
+		tests: [
+			{
+				input: `
+.F----7F7F7F7F-7....
 .|F--7||||||||FJ....
 .||.FJ||||||||L7....
 FJL7L7LJLJ||LJ.L-7..
@@ -123,10 +111,10 @@ L--J.L7...LJS7F-7L7.
 .....|FJLJ|FJ|F7|.LJ
 ....FJL-7.||.||||...
 ....L---J.LJ.LJLJ...`,
-      expected: 8,
-       },
-      {
-        input:`
+				expected: 8,
+			},
+			{
+				input: `
 FF7FSF7F7F7F7F7F---7
 L|LJ||||||||||||F--J
 FL-7LJLJ||||||LJL-77
@@ -137,11 +125,11 @@ L---JF-JLJ.||-FJLJJ7
 7-L-JL7||F7|L7F-7F7|
 L.L7LFJ|||||FJL7||LJ
 L7JLJL-JLJLJL--JLJ.L`,
-        expected:10
-      }
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: true,
+				expected: 10,
+			},
+		],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	onlyTests: false,
 })
